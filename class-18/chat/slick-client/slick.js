206e2848e14eb00d1c10c994281db58938ec7309
Rawan <rawan.nujoom@gmail.com>
2020-06-16 20:01:54 +0300

    add comments

--- class-18/chat/slick-client/slick.js
+++ class-18/chat/slick-client/slick.js
@@ -1,80 +1,67 @@
-// CLIENT
 const inquirer = require('inquirer');
 const io = require('socket.io-client');
+//connect to slick namespace
 const slick = io.connect('http://localhost:3000/slick');
 
-let messages = [];
-let activeInput = false;
-let channel = 'general';
+const messages = [];
 let name = '';
-
-// We're going to use the variable "channel" to let us move (reconnect)
-// between rooms in the "slick" namespace on our server
-// emit join event that slick app in server is listening to
+let activeInput = true;
+let channel = 'general'; 
 slick.emit('join', channel);
 
-// Update our global channel variable, and getting the channel
+
+// Update our global channel variable
 slick.on('joined', (joinedChannel) => {
-  console.log("on joined !! update")
   channel = joinedChannel;
+  beSlick();
 });
 
 // When the server sends out a list of all messages, render them
-slick.on('message', payload => {
+slick.on('message', (payload) => {
   console.clear();
   messages.push(payload);
-  messages.forEach(message => console.log(message))
+  messages.forEach((message) => console.log(message));
   console.log('');
-  beSlick();
 });
 
 // Get user input
 async function beSlick() {
-
-  // Need to check/set a flag, 
-  // otherwise we'll recursively call this async function on every reload
-  if (activeInput) { return; }
+  // Need to check/set a flag, otherwise we'll recursively call this async function on every reload
+  if (activeInput) {
+    return;
+  }
   activeInput = true;
-
   const response = await inquirer.prompt([
     {
       prefix: '',
       name: 'text',
-      message: `-----------------------------------\n ${channel} >`
-    }
+      message: `----------------\n ${channel}`,
+    },
   ]);
 
-  // Pull out the first 4 chars of the input to find exit or join
-  let command = response.text.toLowerCase().split('', 4).join('');
-  // either deal with the command, or send the message
+  const command = response.text.toLowerCase().split(' ')[0];
   switch (command) {
     case 'quit':
       process.exit();
-      break;
     case 'join':
-      let room = response.text.split(' ')[1] || channel; // default to the current channel
+      const room = response.text.toLowerCase().split(' ')[1];
       activeInput = false;
       slick.emit('join', room);
-      beSlick();
       break;
     default:
       activeInput = false;
-      slick.emit('message', response.text)
+      slick.emit('message', `[${name}]: ${response.text}`);
       beSlick();
       break;
   }
 }
-
 async function getName() {
-  // ask user to first enter his name.
-  // then the user can join the group
-  console.clear(); //clean the console
+  console.clear();
   const input = await inquirer.prompt([
     { name: 'name', message: 'What is your name?' },
   ]);
-  name = input.name;// update golbal name
+  name = input.name;
   activeInput = false;
-  // allow user to insert input
   beSlick();
 }
 getName();
