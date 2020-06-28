a0d080fcdce0d114e8a4f1b88ae43a067c0f86a4
Rawan <rawan.nujoom@gmail.com>
2020-06-28 11:51:47 +0300

    react-demo init

---
+++ class-26/react-demo/src/app.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>New header</h1>
+        </header>
+    )
+};
+
+const Footer =() => <footer>&copy;2020 footer</footer>;
+
+// Main 
+
+//class App
+class App extends React.Component {
+    render() {
+        return (
+            // I will add Header, Main, Footer
+        )
+    }
+}
+
+export default App;
+
---
+++ class-26/react-demo/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+// import App from app.js 
+
+class Main extends React.Component {
+    render() {
+        // return App
+    }
+}
+
+
+const rootElement = document.getElementById("root");
+ReactDom.render(<Main />, rootElement);
