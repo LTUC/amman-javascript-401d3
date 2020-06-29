e563190e4750a36a2a4daaa07a09cb2fd25037bd
Rawan <rawan.nujoom@gmail.com>
2020-06-29 11:37:38 +0300

    Updates

---
+++ precache-manifest.4b196cb839dc8b23eb420c85f61e791b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35614e163936891a0e30391974212b73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa4eacdd53f5ecbc01b6",
+    "url": "/static/css/main.e7080dbf.chunk.css"
+  },
+  {
+    "revision": "715ce0b1b07f48a73a0a",
+    "url": "/static/js/2.a0310612.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a0310612.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa4eacdd53f5ecbc01b6",
+    "url": "/static/js/main.8edeae2b.chunk.js"
+  },
+  {
+    "revision": "13b1ab0d619432ec0b72",
+    "url": "/static/js/runtime-main.fbc927c0.js"
+  }
+]);
